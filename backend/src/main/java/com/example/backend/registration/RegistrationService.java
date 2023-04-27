a494476a5c6c50316789e28c0af9dd5ddc1fd79a
Catta <catalincatta@yahoo.com>
2023-04-27 11:31:37 +0300

    added registration

---
+++ backend/src/main/java/com/example/backend/registration/RegistrationService.java
@@ -0,0 +1,31 @@
+package com.example.backend.registration;
+
+import com.example.backend.model.appuser.AppUser;
+import com.example.backend.model.appuser.AppUserRole;
+import com.example.backend.model.appuser.AppUserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class RegistrationService {
+
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
+
+    public String register(RegistrationRequest request){
+        boolean isValidEmail = emailValidator.test(request.email());
+        if (!isValidEmail){
+            throw new IllegalStateException("email not valid");
+        }
+        return appUserService.signUpUser(
+                new AppUser(
+                        request.firstName(),
+                        request.lastName(),
+                        request.email(),
+                        request.password(),
+                        AppUserRole.USER
+                )
+        );
+    }
+}
